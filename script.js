52a6c9bd1d2dfe33814f7b48f689e7831353f9b4
shmarik <smarovartem01@gmail.com>
2023-03-12 02:15:21 +0700

    lol

--- script.js
+++ script.js
@@ -25,11 +25,3 @@ function removeTodoEl() {
         }
     }
 }
-
-const burger = document.querySelector(".burger")
-const nav = document.querySelector(".burger-nav")
-
-burger.addEventListener("click",() =>{
-    nav.classList.toggle("nav--active")
-})
-
